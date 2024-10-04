64de810c94c28902e1d342cae95fa4b1513157e9
suojae <suojae.developer@gmail.com>
2024-10-04 14:26:43 +0900

    limit 테스트 통과 코드 작성

--- bin/main.js
+++ bin/main.js
@@ -3,15 +3,27 @@ var app = express();
 var morgan = require('morgan');
 
 var users = [
-    {id: 1, name: 'alice'},
-    {id: 2, name: 'bob'},
-    {id: 3, name: 'bobhorn'},
+    { id: 1, name: 'alice' },
+    { id: 2, name: 'bob' },
+    { id: 3, name: 'bobhorn' },
 ];
 
 app.use(morgan('dev'));
 
 app.get('/users', function (req, res) {
-    const limit = req.query.limit
+    const limit = parseInt(req.query.limit, 10);
+
+    // limit이 제공되지 않으면 users 전체를 반환
+    if (!req.query.limit) {
+        return res.json(users);
+    }
+
+    // limit이 숫자가 아니면 400 반환
+    if (Number.isNaN(limit)) {
+        return res.status(400).end();
+    }
+
+    // limit에 맞는 users 반환
     res.json(users.slice(0, limit));
 });
 
