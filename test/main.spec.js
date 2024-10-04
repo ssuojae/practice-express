a9f3dcd497fd008a62b1d7c65253ca637d07948a
suojae <suojae.developer@gmail.com>
2024-10-04 15:23:57 +0900

    id delete 실패 테스트 케이스 추가

--- test/main.spec.js
+++ test/main.spec.js
@@ -55,10 +55,21 @@ describe('GET /users/:id', () => {
 });
 
 describe('GET /users/1', () => {
-    it('204를 응답한다', (done) => {
-        request(app)
-            .delete('/users/1')
-            .expect(204)
-            .end(done);
+    describe('성공시', () => {
+        it('204를 응답한다', (done) => {
+            request(app)
+                .delete('/users/1')
+                .expect(204)
+                .end(done);
+        })
+    })
+
+    describe('실패시', () => {
+        it('id가 숫자가 아닐경우 400으로 응답한다', (done) => {
+            request(app)
+                .delete('/users/one')
+                .expect(409)
+                .end(done);
+        })
     })
 });
